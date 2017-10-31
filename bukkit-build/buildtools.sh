f1ac9d1b5b048a6683696f947f7aefef477089b8
toxuin <otstoypuksa@ya.ru>
2017-10-30 19:33:09 -0700

    Always build against java 8 now

--- bukkit-build/buildtools.sh
+++ bukkit-build/buildtools.sh
@@ -2,7 +2,7 @@
 file=bukkit.jar
 if [ ! -f $file ]; then
     mkdir build && cd build
-    wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar && java -jar BuildTools.jar
+    wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar && java -jar BuildTools.jar --rev $BUKKIT_VER
     cp craftbukkit-* ../"$file"
-    cd .. && rm -r build
+    cd .. && rm -rf build
 fi
